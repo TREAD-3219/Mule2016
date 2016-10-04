f39d70935ffeeb5b3e7a1a14a0d643287edf357a
Freebjoe000 <FREEBJOE000@auburn.wednet.edu>
2016-10-04 15:49:29 -0700

    A simple drive forward command

---
+++ src/MyRobot/commands/Utopia.java
@@ -0,0 +1,34 @@
+package MyRobot.commands;
+
+import MyRobot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Utopia extends Command {
+
+	@Override
+	protected void end() {
+		Robot.drive.driveValues(0, 0);
+	}
+
+	@Override
+	protected void execute() {
+		
+	}
+
+	@Override
+	protected void initialize() { 
+		Robot.drive.driveValues(15, 0);
+		this.setTimeout(5);
+	}
+
+	@Override
+	protected void interrupted() {
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return this.isTimedOut();
+	}
+
+}
