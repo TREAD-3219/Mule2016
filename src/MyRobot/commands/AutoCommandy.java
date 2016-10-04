9288c73612f509102caa4e313a634827042b925e
JonathanRiggins <jonathanriggins6924@gmail.com>
2016-10-04 15:49:32 -0700

    Yeah Drive Stuffs

---
+++ src/MyRobot/commands/AutoCommandy.java
@@ -0,0 +1,36 @@
+package MyRobot.commands;
+
+import MyRobot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoCommandy extends Command {
+
+	@Override
+	protected void end() {
+		Robot.drive.driveValues(0, 0);
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initialize() {
+		Robot.drive.driveValues(10, 0);
+		this.setTimeout(1);
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return this.isTimedOut();
+	}
+
+}
