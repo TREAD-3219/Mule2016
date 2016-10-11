cdacb42afb592a872e7317f1f59c7260718acdb3
Freebjoe000 <joelwfreeborn@hotmail.com>
2016-10-11 16:02:53 -0700

    change to looptyloop

--- src/MyRobot/commands/looptyloop.java
+++ src/MyRobot/commands/looptyloop.java
@@ -1,5 +1,9 @@
 package MyRobot.commands;
 
-public class looptyloop {
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
+public class looptyloop extends CommandGroup {
+public looptyloop() {
+	this.addSequential(new AutonomousCommand());
+}
 }
